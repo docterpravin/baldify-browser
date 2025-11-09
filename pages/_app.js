36c7565510f155bbab13a1bf6cf53ef846dc6d62
docterpravin <156455449+docterpravin@users.noreply.github.com>
2025-11-09 10:57:32 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,3 +2,11 @@ import '../styles.css';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+// pages/_app.js
+
+// ✅ Correct path (since styles.css is in project root)
+import '../styles.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
